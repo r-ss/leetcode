d05b6b849a93cf2a9d1cff20940df4a15b5699ce
Ress <mail@ress.ws>
2022-02-20 08:54:03 +0300

    save2

--- 23_merge_k_sorted_lists.py
+++ 23_merge_k_sorted_lists.py
@@ -42,6 +42,7 @@ The sum of lists[i].length won't exceed 10^4.
 
 """
 
+
 from typing import Optional, List
 
 # Definition for singly-linked list.
@@ -53,12 +54,36 @@ class ListNode:
 
 class Solution:
     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
-
-        # TODO - Solution
+        if not len(lists):
+            return None
         
-        pass
-
+        while len(lists) > 1:
+            newLists = [] 
+            for i in range(0, len(lists), 2):
+                LL1 = lists[i]
+                LL2 = lists[i + 1] if (i + 1) < len(lists) else None
+                mergedList = self.merge2List(LL1, LL2)
+                newLists.append(mergedList)
+            lists = newLists
+        return lists[0]
+                
+    def merge2List(self, l1, l2):
+        dummy = ListNode()
+        current = dummy
+        while l1 and l2:
+            if l1.val < l2.val:
+                current.next = l1
+                l1 = l1.next
+            else:
+                current.next = l2
+                l2 = l2.next
+            current = current.next
+        if l1:
+            current.next = l1
+        if l2:
+            current.next = l2
+        return dummy.next
 
 
 def test_solution():
-    assert Solution().mergeKLists(lists = [[1,4,5],[1,3,4],[2,6]]) == [1,1,2,3,4,4,5,6]
+    assert Solution().mergeKLists(lists = [ll([1,4,5]),ll([1,3,4]),ll([2,6])]) == [1,1,2,3,4,4,5,6]
